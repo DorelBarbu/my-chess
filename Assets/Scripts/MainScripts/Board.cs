3121a9709aa0c36208054893c49a706211d946b9
Dorel Barbu <dorelbarbu96@gmail.com>
2020-01-18 22:39:14 +0200

    Added board script

---
+++ Assets/Scripts/MainScripts/Board.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private Dictionary<string, Square> SquareMapping = new Dictionary<string, Square>();
+    private void Awake()
+    {
+        MapSquares();
+    }
+
+    private void MapSquares()
+    {
+        Square[] squares = FindObjectsOfType<Square>();
+        foreach(Square square in squares)
+        {
+            SquareMapping.Add(square.gameObject.name, square);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
